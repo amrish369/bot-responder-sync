a007e6bf59c1f6e72fd47508fc8871b174c75118
AMRISH YADAV <98892580+amrish369@users.noreply.github.com>
2026-05-27 22:13:26 +0530

    config.server.ts

--- src/lib/telegram/config.server.ts
+++ src/lib/telegram/config.server.ts
@@ -6,7 +6,7 @@ const required = (name: string, fallback?: string) => {
 
 export const BOT_TOKEN = () => required("BOT_TOKEN");
 export const TMDB_API_KEY = () => required("TMDB_API_KEY");
-export const CHANNEL = () => process.env.CHANNEL || "@cineradarai";
+export const CHANNEL = () => process.env.CHANNEL || "@cinebotbackupgroup";
 export const CHANNEL_USERNAME = () => (process.env.CHANNEL || "@cineradarai").replace("@", "");
 export const BOT_USERNAME = () => process.env.BOT_USERNAME || "cineradarai_bot";
 
